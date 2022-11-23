1f4c41c8d0bea34aa56ff1e0a0d8ceb5af3847b6
Georgy <i@64mb.ru>
2022-11-23 23:17:38 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:fluent_ui/fluent_ui.dart' hide Page;
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FluentApp(
+      title: 'Pilot S3',
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        accentColor: Colors.blue,
+        visualDensity: VisualDensity.standard,
+        focusTheme: FocusThemeData(
+          glowFactor: is10footScreen() ? 2.0 : 0.0,
+        ),
+      ),
+      theme: ThemeData(
+        accentColor: Colors.blue,
+        visualDensity: VisualDensity.standard,
+        focusTheme: FocusThemeData(
+          glowFactor: is10footScreen() ? 2.0 : 0.0,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {'/': (context) => const MyHomePage()},
+    );
+    ;
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool value = false;
+
+  int index = 0;
+
+  final key = GlobalKey();
+  final searchFocusNode = FocusNode();
+  final searchController = TextEditingController();
+  void resetSearch() => searchController.clear();
+  String get searchValue => searchController.text;
+  final List<NavigationPaneItem> originalItems = [
+    PaneItemHeader(header: const Text('Form')),
+    PaneItemSeparator(),
+    PaneItem(
+      icon: const Icon(FluentIcons.home),
+      title: const Text('Home'),
+      body: Text('123'),
+    ),
+  ];
+  final List<NavigationPaneItem> footerItems = [
+    PaneItemSeparator(),
+    PaneItem(
+      icon: const Icon(FluentIcons.settings),
+      title: const Text('Settings'),
+      body: Text('1'),
+    ),
+    // TODO: mobile widgets, Scrollbar, BottomNavigationBar, RatingBar
+  ];
+  late List<NavigationPaneItem> items = originalItems;
+
+  @override
+  void initState() {
+    searchController.addListener(() {
+      setState(() {
+        if (searchValue.isEmpty) {
+          items = originalItems;
+        } else {
+          items = [...originalItems, ...footerItems]
+              .whereType<PaneItem>()
+              .where((item) {
+                assert(item.title is Text);
+                final text = (item.title as Text).data!;
+                return text.toLowerCase().contains(searchValue.toLowerCase());
+              })
+              .toList()
+              .cast<NavigationPaneItem>();
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FluentTheme.of(context);
+    return NavigationView(
+      pane: NavigationPane(
+        selected: () {
+          // if not searching, return the current index
+          if (searchValue.isEmpty) return index;
+
+          final indexOnScreen = items.indexOf(
+            [...originalItems, ...footerItems]
+                .whereType<PaneItem>()
+                .elementAt(index),
+          );
+          if (indexOnScreen.isNegative) return null;
+          return indexOnScreen;
+        }(),
+        onChanged: (i) {
+          // If searching, the values will have different indexes
+          if (searchValue.isNotEmpty) {
+            final equivalentIndex = [...originalItems, ...footerItems]
+                .whereType<PaneItem>()
+                .toList()
+                .indexOf(items[i] as PaneItem);
+            i = equivalentIndex;
+          }
+          resetSearch();
+          setState(() => index = i);
+        },
+        indicator: EndNavigationIndicator(),
+        items: items,
+        header: SizedBox(height: 10.0),
+        autoSuggestBox: TextBox(
+          key: key,
+          controller: searchController,
+          placeholder: 'Search',
+          focusNode: searchFocusNode,
+        ),
+        autoSuggestBoxReplacement: const Icon(FluentIcons.search),
+        footerItems: searchValue.isNotEmpty ? [] : footerItems,
+      ),
+      onOpenSearch: () {
+        searchFocusNode.requestFocus();
+      },
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
