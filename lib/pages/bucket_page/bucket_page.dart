75fc8269a508834ebaa41ba953909317311f29f7
Georgy <i@64mb.ru>
2023-03-27 00:00:20 +0300

    feat: download file

--- lib/pages/bucket_page/bucket_page.dart
+++ lib/pages/bucket_page/bucket_page.dart
@@ -1,10 +1,17 @@
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:minio/io.dart';
+import 'package:minio/minio.dart';
 import 'package:minio/models.dart';
 import 'package:pilot_s3/models/connection.dart';
 import 'package:pilot_s3/pages/bucket_page/bloc/bucket_page_bloc.dart';
 import 'package:pilot_s3/storage.dart';
 import 'package:file_icon/file_icon.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:path/path.dart' as path_lib;
 
 class BucketPage extends StatelessWidget {
   BucketPage(
@@ -70,7 +77,36 @@ class BucketPage extends StatelessWidget {
                         size: 22,
                       ),
                       title: Text(objectName),
-                      onPressed: () {},
+                      onPressed: () async {
+                        var downloadDir =
+                            await path_provider.getDownloadsDirectory();
+                        String? selectedDirectory = await FilePicker.platform
+                            .getDirectoryPath(
+                                initialDirectory: downloadDir?.path ?? '/');
+
+                        Minio minio = Minio(
+                            endPoint: connection.endpoint,
+                            accessKey: connection.accessKey,
+                            secretKey: connection.secretKey);
+
+                        await minio.fGetObject(
+                            bucket.name,
+                            object.key!,
+                            path_lib.join(selectedDirectory!,
+                                path_lib.basename(object.key!)));
+
+                        displayInfoBar(context, builder: (context, close) {
+                          return InfoBar(
+                            title: const Text('File downloaded'),
+                            content: Text(object.key!),
+                            action: IconButton(
+                              icon: const Icon(FluentIcons.clear),
+                              onPressed: close,
+                            ),
+                            severity: InfoBarSeverity.success,
+                          );
+                        });
+                      },
                     ));
                   }
                 },
