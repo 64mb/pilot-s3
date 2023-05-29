47db5fa70deb8d25e6262d144ed7caaef242b938
AlexEbenrode <38831366+AlexEbenrode@users.noreply.github.com>
2023-05-29 18:24:11 +0500

    feat: dashboard (#13)

---
+++ lib/widgets/home_dashboard.dart
@@ -0,0 +1,53 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:pilot_s3/storage.dart';
+import 'package:pilot_s3/widgets/dashboard_card.dart';
+
+class HomeDashboard extends StatelessWidget {
+  const HomeDashboard({super.key, required this.storage});
+
+  final Storage storage;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: storage.getDashboardStatistic(),
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? SingleChildScrollView(
+                  child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          DashboardCard(
+                            count: snapshot.data!['connections']!,
+                            label: snapshot.data!['connections'] == 1
+                                ? 'Connection'
+                                : 'Connections',
+                          ),
+                          const SizedBox(
+                            width: 24,
+                          ),
+                          DashboardCard(
+                            count: snapshot.data!['buckets']!,
+                            label: snapshot.data!['buckets'] == 1
+                                ? 'Bucket'
+                                : 'Buckets',
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ))
+              : const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Center(
+                    child: ProgressRing(),
+                  ),
+                );
+        });
+  }
+}
