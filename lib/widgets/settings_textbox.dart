4da0f3cef185f4253a128e40f7737378f4f037d6
Konstantin Marchenko <kossdm@gmail.com>
2023-06-07 21:24:17 +0300

    fix: Removed Material dependency

--- lib/widgets/settings_textbox.dart
+++ lib/widgets/settings_textbox.dart
@@ -1,21 +1,43 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 
-class SettingsTextBox extends StatelessWidget {
+class SettingsTextBox extends StatefulWidget {
   const SettingsTextBox(
-      {super.key, this.label = '', this.value = '', required this.onChanged});
-
+      {Key? key, this.label = '', this.value = '', required this.onChanged})
+      : super(key: key);
   final String label;
   final String value;
   final Function(String) onChanged;
 
   @override
+  StateSettingsTextBox createState() => StateSettingsTextBox();
+}
+
+class StateSettingsTextBox extends State<SettingsTextBox> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    _controller.text = widget.value;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-        child: TextFormField(
-      onChanged: onChanged,
-      initialValue: value,
-      decoration:
-          InputDecoration(border: const OutlineInputBorder(), labelText: label),
-    ));
+    return TextBox(
+      controller: _controller,
+      onChanged: widget.onChanged,
+      prefix: Row(children: [
+        const SizedBox(
+          width: 8,
+        ),
+        Text(
+          widget.label,
+          style: const TextStyle(
+              fontSize: 14, color: Color.fromARGB(255, 150, 150, 150)),
+        )
+      ]),
+      //onChanged: onChanged,
+      expands: false,
+    );
   }
 }
