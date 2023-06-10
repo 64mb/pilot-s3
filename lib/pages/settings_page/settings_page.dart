403cc2de39e4c08a64aea824393176a1b3554f8e
Alex Ebenrode <alexebenrode@gmail.com>
2023-06-10 11:10:59 +0500

    feat:added deletion confirmation

--- lib/pages/settings_page/settings_page.dart
+++ lib/pages/settings_page/settings_page.dart
@@ -22,7 +22,29 @@ class SettingsPage extends StatelessWidget {
       Button(
         child: const Text('Delete connection'),
         onPressed: () async {
-          storage.deleteConnection(connection);
+          showDialog<String>(
+              context: context,
+              builder: (context) => ContentDialog(
+                    title: const Text('Delete file permanently?'),
+                    content: const Text(
+                      'If you delete this file, you won\'t be able to recover it. Do you want to delete it?',
+                    ),
+                    actions: [
+                      Button(
+                        onPressed: () {
+                          storage.deleteConnection(connection);
+                          Navigator.pop(context, 'User deleted file');
+                        },
+                        child: const Text('Delete'),
+                      ),
+                      Button(
+                        onPressed: () {
+                          Navigator.pop(context, 'User canceled dialog');
+                        },
+                        child: const Text('Cancel'),
+                      )
+                    ],
+                  ));
         },
       ),
       const SizedBox(
