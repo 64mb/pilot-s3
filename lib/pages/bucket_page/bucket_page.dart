900f51ea211b0440aaef8e2f1cb3d826f576152a
AlexEbenrode <38831366+AlexEbenrode@users.noreply.github.com>
2023-06-17 17:02:13 +0500

    feat: toolbar path chips navigation (#32)

--- lib/pages/bucket_page/bucket_page.dart
+++ lib/pages/bucket_page/bucket_page.dart
@@ -13,6 +13,7 @@ import 'package:file_icon/file_icon.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:path/path.dart' as path_lib;
+import 'package:path/path.dart' as path;
 import 'package:pilot_s3/widgets/bucket_toolbar.dart';
 
 part 'bucket_page_elements.dart';
@@ -42,10 +43,6 @@ class BucketPage extends StatelessWidget {
               previous.status != current.status;
         },
         builder: (context, state) {
-          String currentPath = state.path.isNotEmpty
-              ? '${state.path.join('/')}/'
-              : state.path.join('/');
-
           List<String>? directories = state.items.prefixes;
           List<Object>? objects = state.items.objects;
           List<ListTile>? directoriesWidgets = [];
@@ -128,7 +125,7 @@ class BucketPage extends StatelessWidget {
               context.read<BucketPageBloc>().add(const ToBack());
               context
                   .read<BucketPageBloc>()
-                  .add(ObjectsRequested(prefix: newPath.join('/')));
+                  .add(ObjectsRequested(prefix: path.joinAll(newPath)));
             },
           );
           if (state.path.isNotEmpty) allTiles.add(backTile);
@@ -136,11 +133,17 @@ class BucketPage extends StatelessWidget {
           allTiles.addAll(objectsWidgets);
 
           BucketToolbar toolbar = BucketToolbar(
-            path: currentPath,
+            path: state.path,
+            onChange: (newPath) {
+              context.read<BucketPageBloc>().add(DirectoryAdded(path: newPath));
+              context
+                  .read<BucketPageBloc>()
+                  .add(ObjectsRequested(prefix: path.joinAll(newPath)));
+            },
             onRefresh: () {
               context
                   .read<BucketPageBloc>()
-                  .add(ObjectsRequested(prefix: state.path.join('/')));
+                  .add(ObjectsRequested(prefix: path.joinAll(state.path)));
             },
             onUpload: uploadObject(state, connection, bucket, context),
           );
